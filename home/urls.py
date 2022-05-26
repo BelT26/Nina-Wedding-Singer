f1ecdaa6896c3a5623c036fffedf363c2d07f811
BelT26 <helen.taylor@hotmail.it>
2022-05-26 14:02:19 +0000

    add views and urls for all pages

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [    
     path('', views.index, name='home'),
+    path('add_song', views.add_song, name='add_song'),
+    path('bio', views.bio, name='bio'),
+    path('contact', views.contact, name='contact'),
+    path('repertoire', views.repertoire, name='repertoire'),
+    path('services', views.services, name='services'),
+    path('testimonials', views.testimonials, name='testimonials')
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,45 @@ def index(request):
     returns the home page
     """
     return render(request, 'home/index.html')
+
+
+def services(request):
+    """
+    returns the services page
+    """
+    return render(request, 'home/services.html')
+
+
+def repertoire(request):
+    """
+    returns the repertoire page
+    """
+    return render(request, 'home/repertoire.html')
+
+
+def testimonials(request):
+    """
+    returns the home page
+    """
+    return render(request, 'home/testimonials.html')
+
+
+def bio(request):
+    """
+    returns the home page
+    """
+    return render(request, 'home/bio.html')
+
+
+def contact(request):
+    """
+    returns the contacts page
+    """
+    return render(request, 'home/contact.html')
+
+
+def add_song(request):
+    """
+    returns a form to add songs to the repertoire
+    """
+    return render(request, 'home/add_song.html')
